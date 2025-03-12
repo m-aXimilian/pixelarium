ea8697088c4e68d7e9dd3beaa45e5a97e366a34f
Kueffner, Maximilian <maximilian.kueffner@zeiss.com>
2025-03-12 18:54:23 +0100

    project boilerplate

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "cmake -B build -S . && cmake --build build"))))
---
+++ cmake/libCZI.cmake
@@ -0,0 +1,23 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  libCZI
+  GIT_REPOSITORY https://github.com/ZEISS/libczi.git
+  GIT_TAG        origin/main
+)
+
+if(NOT libCZI_POPULATED)
+  message(STATUS "Fetching libCZI")
+  FetchContent_Populate(libCZI)
+
+  set(LIBCZI_BUILD_CZICMD OFF CACHE BOOL "" FORCE)
+  set(LIBCZI_BUILD_DYNLIB OFF CACHE BOOL "" FORCE)
+  set(LIBCZI_BUILD_UNITTESTS OFF CACHE BOOL "" FORCE)
+  set(LIBCZI_DO_NOT_SET_MSVC_RUNTIME_LIBRARY  ON CACHE BOOL "" FORCE)  
+
+  add_subdirectory(${libczi_SOURCE_DIR} ${libczi_BINARY_DIR})
+endif()
+
+FetchContent_GetProperties(libCZI)
+set(LIBCZI_INCLUDE_DIR "${libczi_SOURCE_DIR}/Src/libCZI")
+message(STATUS ${LIBCZI_INCLUDE_DIR})
