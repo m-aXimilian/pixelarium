4ace9d9eeac185893d1cd084f41cd39c8a88dafa
Maximilian Kueffner <kueffnermax@gmail.com>
2025-09-24 19:54:18 +0200

    use awesomeDoxygen

---
+++ cmake/awesomeDoxygen.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+FetchContent_Declare(
+    doxygen-awesome-css
+    URL https://github.com/jothepro/doxygen-awesome-css/archive/refs/heads/main.zip
+)
+FetchContent_MakeAvailable(doxygen-awesome-css)
+
+# Save the location the files were cloned into
+# This allows us to get the path to doxygen-awesome.css
+FetchContent_GetProperties(doxygen-awesome-css SOURCE_DIR AWESOME_CSS_DIR)
